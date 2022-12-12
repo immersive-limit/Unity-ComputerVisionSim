d266e7152ccc309155f95173c894227b3dd1ad4e
maki <haru.maki.ch.0303@gmail.com>
2022-12-12 14:28:01 +0900

    add move objects (#1)

---
+++ Unity/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    void Update() {
+
+        // transformを取得
+        Transform myTransform = this.transform;
+
+        // 座標を取得
+        Vector3 pos = myTransform.position;
+        pos.x += 0.01f;    // x座標へ0.01加算
+        // pos.y += 0.01f;    // y座標へ0.01加算
+        // pos.z += 0.01f;    // z座標へ0.01加算        
+        myTransform.position = pos;  // 座標を設定
+    }
+}
